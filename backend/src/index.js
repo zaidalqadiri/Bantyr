e9fea9eb50b8d61175f86f75b2bb99456181301e
Zaid <zaidalqadiri@gmail.com>
2024-12-31 14:47:17 -0700

    created src folders for backend and created routes for authentication

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,7 +1,14 @@
 import express from "express"
+import dotenv from "dotenv"
+import authRoutes from "./routes/auth.route.js"
 
+dotenv.config()
 const app = express();
 
-app.listen(5001, () => {
+const PORT = process.env.PORT
+
+app.use("/api/auth", authRoutes)
+
+app.listen(PORT, () => {
     console.log("server is running on port 5001")
 });
