35f6583a971a59c1afca3e4557df03c137ae0bd9
Zaid <zaidalqadiri@gmail.com>
2025-01-01 12:58:32 -0700

    implemented logout

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -83,5 +83,12 @@ export const login = async (req, res) => {
 };
 
 export const logout = (req, res) => {
-    res.send("logout route")
+   try {
+    res.cookie("jwt", "", {maxAge:0})  // clear cookies
+    res.status(200).json({ message: "Logged out successfully" })
+
+   } catch (error) {
+    console.log("Error in logout controller", error.message)
+    res.status(500).json({ message: "internal Server Error" })
+   }
 };
--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -4,9 +4,7 @@ import { login, logout, signup } from "../controllers/auth.controller.js"
 const router = express.Router()
 
 router.post("/signup", signup)
-
 router.post("/login", login)
-
 router.post("/logout", logout)
 
 export default router;
