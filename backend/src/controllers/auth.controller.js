bee0f1411296e5c9ed4a16daeb5423b008f21302
Zaid <zaidalqadiri@gmail.com>
2024-12-31 23:32:08 -0700

    setup the database and implemented few signup features

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,5 +1,41 @@
-export const signup = (req, res) => {
-    res.send("signup route")
+import User from "../models/user.model.js"
+import bcrypt from "bcryptjs"
+
+export const signup = async (req, res) => {
+    const {fullName,email,password} = req.body
+
+    try {
+        if (password.length < 6) {
+            return res.status(400).json({ message: "password must be at least 6 characters" });
+        }
+
+        const user = await User.findOne({email})
+
+        if (user) return res.status(400).json({ message: "Email already exists"})
+
+        // hash password
+        const salt = await bcrypt.genSalt(10)
+        const hashedPassword = await bcrypt.hash(password, salt)
+
+        const newUser = new User({
+            fullName,
+            email,
+            password:hashedPassword
+        })
+
+        if(newUser) {
+            // generate jwt token here
+            
+
+        } else {
+            res.status(400).json({ message: "invalud user data" });
+        }
+
+
+    } catch(error) {
+
+    }
+   
 };
 
 export const login = (req, res) => {
--- backend/src/index.js
+++ backend/src/index.js
@@ -1,14 +1,18 @@
 import express from "express"
 import dotenv from "dotenv"
 import authRoutes from "./routes/auth.route.js"
+import { connectDB } from "./lib/db.js"
 
 dotenv.config()
 const app = express();
 
 const PORT = process.env.PORT
 
+app.use(express.json());
+
 app.use("/api/auth", authRoutes)
 
 app.listen(PORT, () => {
-    console.log("server is running on port 5001")
+    console.log("server is running on port: " + PORT);
+    connectDB()
 });
