6aa871410942fcc24f815157c61d18ac39c424a2
Zaid <zaidalqadiri@gmail.com>
2025-01-02 16:32:14 -0700

    created pages and routes

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,10 @@
+import daisyui from "daisyui";
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js,jsx,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [daisyui],
+};
