51ec380db826a055ace322a167eb70a1fc18245a
Zaid <zaidalqadiri@gmail.com>
2025-01-03 23:54:46 -0700

    implemented the login and profile pages

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -36,6 +36,44 @@ export const useAuthStore = create((set) => ({
         } finally {
             set ({ isSigningUp: false })
         }
+    },
+
+    login: async (data) => {
+        set({ isLoggingIn: true })
+        try {
+            const res = await axiosInstance.post("/auth/login", data);
+            set({ authUser: res.data })
+            toast.success("Logged in successfully")
+        } catch (error) {
+            toast.error(error.response.data.message)
+        } finally {
+            set({ isLoggingIn: false })
+        }
+    },
+
+    logout: async () => {
+        try {
+            await axiosInstance.post("/auth/logout")
+            set({ authUser: null })
+            toast.success("Logged out successfully")
+        } catch (error) {
+            toast.error(error.response.data.message)
+            
+        }
+    },
+
+    updateProfile: async (data) => {
+        set({ isUpdatingProfile: true })
+        try {
+            const res = await axiosInstance.put("/auth/update-profile", data)
+            set({ authUser: res.data })
+            toast.success("Profile Updated successfully")
+        } catch (error) {
+            console.log("error in updating profile:", error)
+            toast.error(error.response.data.message)
+        } finally {
+            set({ isUpdatingProfile: false })
+        }
     }
 
 }))
