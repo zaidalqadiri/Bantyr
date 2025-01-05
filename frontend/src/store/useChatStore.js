96066b93f7ba5fc78bc1c3e1164712448df76b9f
Zaid <zaidalqadiri@gmail.com>
2025-01-04 22:27:22 -0700

    implemented the useChatStore.js file used in the homepage

---
+++ frontend/src/store/useChatStore.js
@@ -0,0 +1,36 @@
+import { create } from "zustand"
+import toast from "react-hot-toast"
+import { axiosInstance } from "../lib/axios"
+
+export const useChatStore = create((set) => ({
+    messages: [],
+    users: [],
+    selectedUser: null,
+    isUsersLoading: false,
+    isMessagesLoading: false,
+
+
+    getUsers: async () => {
+        set({ isUsersLoading: true });
+        try {
+            const res = await axiosInstance.get("/messages/users")
+            set({ users: res.data})
+        } catch (error) {
+            toast.error(error.response.data.message)
+        } finally {
+            set({ isUsersLoading: false })
+        }
+    },
+
+    getMessages: async (userId) => {
+        set({ isMessagesLoading: true })
+        try {
+            const res = await axiosInstance.get(`/messages/${userId}`)
+            set({ messages: res.data })
+        } catch (error) {
+            toast.error(error.response.data.message)
+        } finally {
+            set({ isMessagesLoading: false })
+        }
+    }
+}))
