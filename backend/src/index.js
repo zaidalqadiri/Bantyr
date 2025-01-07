dc2394f6616ae2b515e82add489bdc79944b817e
Zaid <zaidalqadiri@gmail.com>
2025-01-06 17:21:21 -0700

    implemented the socket.io server on top of the current one used

--- backend/src/index.js
+++ backend/src/index.js
@@ -5,9 +5,9 @@ import { connectDB } from "./lib/db.js"
 import cookieParser from "cookie-parser"
 import messageRoutes from "./routes/message.route.js"
 import cors from "cors"
+import {app, server } from "./lib/socket.js"
 
 dotenv.config()
-const app = express();
 
 const PORT = process.env.PORT
 
@@ -21,7 +21,7 @@ app.use(cors({
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
     console.log("server is running on port: " + PORT);
     connectDB()
 });
