4a90af5fc19c169e9e5dabe08578d5b38698810d
Zaid <zaidalqadiri@gmail.com>
2025-01-07 22:24:38 -0700

    fixed bug in development of website

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios"
 
 export const axiosInstance = axios.create({
-    baseURL: import.meta.env.MODE === "developement" ? "http://localhost:5001/api" : "/api", 
+    baseURL: import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "/api", 
     withCredentials: true  // send cookies for all requests
 });
