e2f5a52d33b47cd328251836face1aabe7ca70e1
Zaid <zaidalqadiri@gmail.com>
2025-01-13 23:20:36 -0700

    Updated the name of the chat application

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -16,7 +16,7 @@ const Navbar = () => {
               <div className="size-9 rounded-lg bg-primary/10 flex items-center justify-center">
                 <MessageSquare className="w-5 h-5 text-primary" />
               </div>
-              <h1 className="text-lg font-bold">Chat app</h1>
+              <h1 className="text-lg font-bold">Bantyr</h1>
             </Link>
           </div>
 
--- frontend/src/components/NoChatSelected.jsx
+++ frontend/src/components/NoChatSelected.jsx
@@ -17,7 +17,7 @@ const NoChatSelected = () => {
         </div>
 
         {/* Welcome Text */}
-        <h2 className="text-2xl font-bold">Welcome to the chat app!</h2>
+        <h2 className="text-2xl font-bold">Welcome to Bantyr!</h2>
         <p className="text-base-content/60">
           Select a conversation from the sidebar to start chatting
         </p>
